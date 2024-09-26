13bf4a5bfdd4def6dc10d359ea120987bc971dcb
hhdg1515 <144937930+hhdg1515@users.noreply.github.com>
2024-09-25 21:41:05 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,37 @@
-let saveEl = document.getElementById("save-el")
-let countEl = document.getElementById("count-el")
-let count = 0
+let counter = document.getElementById("count-element");
+let saver = document.getElementById("save-element");
+
+let count = 0;
 
 function increment() {
-    count += 1
-    countEl.textContent = count
+  count += 1;
+  counter.innerText = count;
+}
+
+function decrement() {
+  count -= 1;
+  counter.innerText = count;
+}
+
+function incrementBig() {
+  count += 10;
+  counter.innerText = count;
+}
+
+function decrementBig() {
+  count -= 10;
+  counter.innerText = count;
 }
 
 function save() {
-    let countStr = count + " - "
-    saveEl.textContent += countStr
-    countEl.
+  let display = ` ( ${count} ) `;
+  saver.innerText += display;
+  counter.innerText = 0;
+  count = 0;
 }
 
+function wipe() {
+  counter.innerText = 0;
+  saver.innerText = `Entries: `;
+  count = 0;
+}
